b65479c45eda1d6428824022c2561631d1195746
arrcristian <cristian.arreola233140@potros.itson.edu.mx>
2025-03-06 19:43:47 -0700

    Avance
    Permite al usuario iniciar sesión, el sistema valida los jwt

--- microservicios/usuario/controllers/UsuarioController.js
+++ microservicios/usuario/controllers/UsuarioController.js
@@ -23,12 +23,20 @@ class UsuarioController {
 
     async loginUsuario(req, res) {
         try {
-            const { usuario, pass } = req.body;
-            const resultado = await UsuarioService.loginUsuario(usuario, pass);
+            const { usuario, contrasena } = req.body;
+            const resultado = await UsuarioService.loginUsuario(usuario, contrasena);
             res.json(resultado);
         } catch (error) {
             console.error("Error en login:", error.message);
             res.status(error.status || 500).json({ message: error.message });
+        }
+    }
+
+    async obtenerPerfil(req, res) {
+        try {
+            res.status(200).json({ message: "Perfil válido", usuario: req.usuario });
+        } catch (error) {
+            res.status(500).json({ message: "Error al obtener perfil" });
         }
     }
 }
--- microservicios/usuario/routes/UsuarioRoutes.js
+++ microservicios/usuario/routes/UsuarioRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const UsuarioController = require('../controllers/UsuarioController');
+const verificarToken = require('../middlewares/authMiddleware'); 
 
 // Endpoints para usuarios
 router.post('/crear', UsuarioController.createUsuario);
 router.get('/', UsuarioController.getAllUsuarios);
 router.post('/login', UsuarioController.loginUsuario);
+router.get('/perfil', verificarToken, UsuarioController.obtenerPerfil);
 
 
 
