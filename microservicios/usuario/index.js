41edc18536b7af3f3501f7270c5e7f548c3dd1b1
arrcristian <cristian.arreola233140@potros.itson.edu.mx>
2025-03-06 18:41:29 -0700

    Correccion

--- microservicios/usuario/index.js
+++ microservicios/usuario/index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const usuarioRoutes = require('./routes/UsuarioRoutes');
+const { initializeDatabase } = require('./config/dbConfig');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const db = require('./config/dbConfig');
 
 // Middlewares
 app.use(cors()); // Permitir solicitudes de diferentes dominios
 app.use(express.json()); // Permitir recibir JSON en las peticiones
 
+
 app.use('/api/usuarios', usuarioRoutes);
 
 app.listen(PORT, () => {
   console.log(`Microservicio de Usuarios corriendo en http://localhost:${PORT}`);
+});
