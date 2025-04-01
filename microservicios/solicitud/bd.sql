a84b09faebd681d1d141c70235cefc425cc798e6
arrcristian <cristian.arreola233140@potros.itson.edu.mx>
2025-03-31 21:48:39 -0700

    HU3: Back end y Front end terminado

--- microservicios/solicitud/bd.sql
+++ microservicios/solicitud/bd.sql
@@ -1,12 +1,46 @@
 CREATE DATABASE IF NOT EXISTS solicitudes_db;
 USE solicitudes_db;
 
-CREATE TABLE IF NOT EXISTS solicitudes (
-    tracking_id VARCHAR(20) PRIMARY KEY,
+CREATE TABLE solicitudes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    clave_rastreo VARCHAR(50) UNIQUE NOT NULL,
     ticket_id INT NOT NULL,
     usuario VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    resolutor VARCHAR(100) NOT NULL,
-    estado ENUM('pendiente', 'en proceso', 'completado') DEFAULT 'pendiente',
-    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    resolutor VARCHAR(100),
+    topico VARCHAR(100) NOT NULL,
+    departamento VARCHAR(100) NOT NULL,
+    fecha_creacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    estado VARCHAR(20) NOT NULL DEFAULT 'pendiente',
+    fecha_actualizacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS historial_estados (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    clave_rastreo VARCHAR(50) NOT NULL,
+    estado ENUM('pendiente', 'en proceso', 'finalizado') NOT NULL,
+    fecha_cambio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (clave_rastreo) REFERENCES solicitudes(clave_rastreo) ON DELETE CASCADE
+);
+
+
+CREATE TABLE contactos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    puesto VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    departamento VARCHAR(100) NOT NULL
+);
+
+DELIMITER //
+CREATE TRIGGER before_update_estado
+BEFORE UPDATE ON solicitudes
+FOR EACH ROW
+BEGIN
+    IF OLD.estado <> NEW.estado THEN
+        INSERT INTO historial_estados (tracking_id, estado, fecha_cambio)
+        VALUES (OLD.tracking_id, NEW.estado, NOW());
+    END IF;
+END;
+//
+DELIMITER ;
--- microservicios/solicitud/routes/solicitudRoutes.js
+++ microservicios/solicitud/routes/solicitudRoutes.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const { crearNuevaSolicitud, procesarRespuestaCorreo, actualizarEstado, cancelar } = require('../controllers/solicitudController');
+const express = require('express')
+const { crearNuevaSolicitud, obtenerSeguimiento, procesarRespuestaCorreo, actualizarEstado, cancelar } = require('../controllers/solicitudController');
 const router = express.Router();
 
 router.post('/crear', crearNuevaSolicitud);
 router.post('/cambiar-estado/:clave_rastreo', actualizarEstado);
 router.post('/cancelar/:clave_rastreo', cancelar);
 router.get('/respuesta', procesarRespuestaCorreo);
+router.get('/seguimiento/:clave_rastreo', obtenerSeguimiento);
+
 
 module.exports = router;
