21fa13a9c9bf4ca7c108f62ca739e688cabd5604
arrcristian <cristian.arreola233140@potros.itson.edu.mx>
2025-03-31 21:31:07 -0700

    Correccion

--- microservicios/solicitud/routes/solicitudRoutes.js
+++ microservicios/solicitud/routes/solicitudRoutes.js
@@ -1,13 +1,4 @@
-const express = require('express');
-const { crearNuevaSolicitud, procesarRespuestaCorreo, actualizarEstado, cancelar } = require('../controllers/solicitudController');
-const router = express.Router();
-
-router.post('/crear', crearNuevaSolicitud);
-router.post('/cambiar-estado/:clave_rastreo', actualizarEstado);
-router.post('/cancelar/:clave_rastreo', cancelar);
-router.get('/respuesta', procesarRespuestaCorreo);
-
-
+const express = require('express')
 const { crearNuevaSolicitud, obtenerSeguimiento, procesarRespuestaCorreo, actualizarEstado, cancelar } = require('../controllers/solicitudController');
 const router = express.Router();
 
