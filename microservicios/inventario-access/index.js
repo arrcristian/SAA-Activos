7abce97235177530362a1382a21a29df5b03263d
arrcristian <cristian.arreola233140@potros.itson.edu.mx>
2025-05-09 17:49:55 -0700

    Validacion de service tag

--- microservicios/inventario-access/index.js
+++ microservicios/inventario-access/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const inventarioRoutes = require('./routes/inventarioRoutes');
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/inventario', inventarioRoutes);
 
 const PORT = process.env.PORT || 3002;
